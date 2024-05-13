a510903a31f72b5efda00a2ecba4926404b1a9e0
ksoo020406 <ksoo020406@gmail.com>
2024-05-14 05:03:07 +0900

    [Add]애니메이션 컨트롤 스크립트 및 컨트롤러 추가

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+// 강의 자료 내용 갈취...
+// 이녀석이 재활용 가능하도록 만들어 줄 거야!
+public class AnimationController : MonoBehaviour
+{
+    protected Animator animator;
+    protected TopDownController controller;
+
+    // TopDownAnimationController 에 Awake override있다!
+    protected virtual void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        controller = GetComponent<TopDownController>();
+    }
+}
---
+++ Assets/Scripts/TopDownAnimationController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TopDownAnimationController : AnimationController
+{
+    // 해쉬 만들기 작업, 해쉬? 문자열 계산 대신 숫자로 받아 읽을 수 있게 하는 처리
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
+    // 너무 조금 움직였을 때는 멈춘 상태로 처리하기 위한 변수
+    private readonly float magnituteThreshold = 0.5f;
+
+    // AnimationController 의 Awake override
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    // 어떤 방향으로 움직이게 될지 백터가 들어오게 될것
+    private void Move(Vector2 vector)
+    {
+        animator.SetBool(IsWalking, vector.magnitude > magnituteThreshold);
+    }
+}
